c5ddec088d27466f8501a1399718fb6ffa3c4ac5
dbs_jd <dbs_jd@users-mbp>
2015-07-12 00:58:25 -0600

    Cambio 1

--- Hoja1/src/Driver/Radio.java
+++ Hoja1/src/Driver/Radio.java
@@ -3,13 +3,50 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Driver;
+
 
 /**
  *
  * @author dbs_jd
  */
-public abstract class Radio implements Interfaz {
-    
+public class Radio implements Interfaz {
+
+	boolean ESTADO, uD;
+	float Band;
+	int Pos = 0;
+	int Emisoras[] = new int[12];
+
+	public void setESTADO(boolean ESTADO){
+		this.ESTADO = ESTADO;
+	}
+	
+	public boolean getEstado(){
+		return ESTADO;
+	}
+	
+	public int getAMFM(){
+		return Pos;
+	}
+	
+	public void setAMFM(int Band){
+		this.Band = Band;
+	}
+	
+	public void Sintonizar(boolean uD){
+		Pos = 2;
+	}
+	
+	public void Guardar(int Pos){
+		Pos = 4;
+	}
+	
+	public void Memoria(int Pos){
+		Pos = 5;
+	}
+
+	public float getEmisora(){
+		return Band;
+	}
+
     
 }
