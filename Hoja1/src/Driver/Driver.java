7c06f9b29e77b4150df7b676050b36de6baf9db7
danielapa <dapocasangre@gmail.com>
2015-07-14 21:19:25 -0600

    Cambio a polimorfismo

--- Hoja1/src/Driver/Driver.java
+++ Hoja1/src/Driver/Driver.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ 
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.InputMismatchException;
