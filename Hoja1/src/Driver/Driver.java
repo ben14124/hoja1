4d6adf0141071c18b4b8f586cadaf3382875ddf1
dbs_jd <dbs_jd@users-mbp>
2015-07-14 21:42:17 -0600

    Arreglados los Try-Catch

--- Hoja1/src/Driver/Driver.java
+++ Hoja1/src/Driver/Driver.java
@@ -19,20 +19,40 @@ public class Driver {
         int posicion;
         int estacion;
         boolean ESTADO=true;
+        int ingreso=0; banda=0; posicion = 0;
         
         RadioCarro Alejuadiela = new RadioCarro();
         
-        Scanner escaner = new Scanner(System.in);
         while (funcionamiento==0){
-            try{
+            //try{
                 System.out.println("\nBienvenido a la Radio. Ingrese una opcion: \nEncender (1) o apagar la radio (2)");
-                int ingreso = escaner.nextInt();
+               boolean try1 = true;
+                while (try1==true){
+                    try {
+                        Scanner escaner = new Scanner(System.in);
+                        ingreso = escaner.nextInt();
+                        try1=false;
+                    }
+                    catch (InputMismatchException e){
+                        System.out.println("Por favor ingrese una opcion valida. \n");
+                    }
+                }
                 if (ingreso==1){
                     Alejuadiela.setESTADO(ESTADO);
                     while (ingreso==1){
 
                         System.out.println("\nQue frecuencia desea seleccionar?: \nAM (1) \nFM (2) \nSalir (3)");
-                        banda = escaner.nextInt();
+                        boolean try2 = true;
+                        while (try2==true){
+                            try {
+                            Scanner escanerbanda = new Scanner(System.in);
+                            banda = escanerbanda.nextInt();
+                            try2=false;
+                            }
+                             catch (InputMismatchException e){
+                                 System.out.println("Por favor ingrese una opcion valida.\n");
+                            }
+                        }
                         if (banda==1){
                             Alejuadiela.setAMFM(0); //Mandamos al objeto un 0 porque seleccionamos AM para que a la variable respectiva  
                                                     //se le asigne 0
@@ -58,13 +78,33 @@ public class Driver {
                             
                             //se pregunta si se desea subir o bajar la primera vez
                             System.out.println("\nDesea subir o bajar?: \nSubir (1) \nBajar (2) \nSalir (3)");
-                            banda = escaner.nextInt();
+                            boolean try3 = true;
+                            while (try3==true){  
+                                try {
+                                    Scanner escanerbanda2 = new Scanner(System.in);
+                                    banda = escanerbanda2.nextInt();
+                                    try3 = false;
+                                }
+                                catch (InputMismatchException e){
+                                    System.out.println("Por favor ingrese una opcion valida.\n");
+                                }
+                            }
                             while (banda!=3){
                                 if (banda==1){ //Mandamos true para subir
                                     System.out.println("\nIngrese 1 si desea seguir subiendo, 2 para bajar y 3 para salir. ");
                                     while (banda==1){
-                                        banda = escaner.nextInt();
-                                        Alejuadiela.Sintonizar(true);
+                                        boolean try4 = true;
+                                        while (try4==true){
+                                            try{
+                                                Scanner escanerbanda3 = new Scanner(System.in);
+                                                banda = escanerbanda3.nextInt();
+                                                Alejuadiela.Sintonizar(true);
+                                                try4 = false;
+                                            }
+                                            catch (InputMismatchException e){
+                                                System.out.println("Por favor ingrese una opcion valida.\n");
+                                            }
+                                        }
                                         
                                     }
                                 }
@@ -73,7 +113,17 @@ public class Driver {
                                     System.out.println("\nIngrese 2 si desea seguir bajando, 1 para subir y 3 para salir. ");
                                     while (banda==2){
                                         Alejuadiela.Sintonizar(false);
-                                        banda = escaner.nextInt();
+                                        boolean try5 = true;
+                                        while (try5==true){
+                                            try{
+                                                Scanner escanerbanda4 = new Scanner(System.in);
+                                                banda = escanerbanda4.nextInt();
+                                                try5 = false;
+                                            }
+                                            catch (InputMismatchException e){
+                                                System.out.println("Por favor ingrese una opcion valida.\n");
+                                            }
+                                        }
                                     }
                                 }
                                 if (banda==3){
@@ -85,11 +135,31 @@ public class Driver {
                             System.out.println("La radio sintonizada es la "+Alejuadiela.getEmisora());
                             
                             System.out.println("\nDesea guardar la radio sintonizada? Si(1). No(2). Salir (3)"); //se pregunta si se desea guardar la radio sintonizada
-                            posicion = escaner.nextInt();
+                            boolean try6 = true;
+                            while (try6==true){
+                                try {
+                                    Scanner escanerposicion = new Scanner(System.in);
+                                    posicion = escanerposicion.nextInt();
+                                    try6 = false;
+                                }
+                                catch (InputMismatchException e){
+                                    System.out.println("Por favor ingrese una opcion valida.\n");
+                                }
+                            }
                             
                             if(posicion == 1){ //si se desea guardar
                                 System.out.println("\nEn que posicion lo desea almacenar? (1-12)"); //pregunta posicion en la que se guarda
-                                posicion = escaner.nextInt();
+                                boolean try7 = true;
+                                while (try7==true){
+                                    try {
+                                        Scanner escanerposicion2 = new Scanner(System.in);
+                                        posicion = escanerposicion2.nextInt();
+                                        try7 = false;
+                                    }
+                                    catch (InputMismatchException e){
+                                        System.out.println("Por favor ingrese una opcion valida.\n");
+                                    }
+                                }
                                 
                                 Alejuadiela.Guardar(posicion); //se guarda en array
                                 Alejuadiela.Memoria(posicion); //se coloca en una variable la emisora almacenada en cierta posicion
@@ -101,11 +171,33 @@ public class Driver {
                             }
                             
                             System.out.println("\nDesea observar alguna de las radios almacenadas en memoria? Si(1). No(2). Salir (3)"); //se pregunta si se desea guardar la radio sintonizada
-                            posicion = escaner.nextInt();
+                            boolean try8 = true;
+                            while (try8==true){
+                                try {
+                                    Scanner escanerposicion3 = new Scanner(System.in);
+                                    posicion = escanerposicion3.nextInt();
+                                    try8 = false;
+                                }
+                                catch (InputMismatchException e) {
+                                    System.out.println("Por favor ingrese una opcion valida.\n");
+
+                                }
+                            }
                             
                             if(posicion == 1){ //si se desea observar
                                 System.out.println("\nQue posicion desea observar? (1-12)"); //pregunta posicion en la que se guarda
-                                posicion = escaner.nextInt();
+                                boolean try9 = true;
+                                while (try9==true){
+                                    try {
+                                        Scanner escanerposicion4 = new Scanner(System.in);
+                                        posicion = escanerposicion4.nextInt();
+                                        try9 = false;
+                                    }
+                                    catch (InputMismatchException e){
+                                        System.out.println("Por favor ingrese una opcion valida.\n");
+
+                                    }
+                                }
                                 
                                 Alejuadiela.Memoria(posicion); //se coloca en una variable la emisora almacenada en cierta posicion
                                 System.out.println("\nLa emisora guardada en la posicion "+posicion+" es "+Alejuadiela.getEmisora()); //se muestra en que posicion se guardó la emisora
@@ -124,12 +216,7 @@ public class Driver {
                     Alejuadiela.setESTADO(ESTADO);
                     funcionamiento = 3;
                 }
-                
-            }
-            catch (InputMismatchException e){
-                System.out.println("Por favor ingrese una opcion valida.\n");
-                int ingreso = escaner.nextInt();
-            }
+
         }
         System.out.println("\nGracias por usar la radio.");
     }
